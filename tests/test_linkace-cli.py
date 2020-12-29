f14211f0b96b821e7adab80151ffd82c38b65e2b
Evan Smith <esincork@gmail.com>
2020-12-29 21:58:08 +0000

    Add basic python project skeleton

---
+++ tests/test_linkace-cli.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import linkace_cli
+
+
+class TestLinkAceCLI(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_dummy_true(self):
+        assert 1 == 1
