38025fd5c60559a38b8e1acf883029abfe8b2ef7
Evan Smith <esincork@gmail.com>
2020-12-31 18:56:48 +0000

    Add help info

--- linkace_cli/cli/search.py
+++ linkace_cli/cli/search.py
@@ -15,6 +15,9 @@ def by_tag(
     exact: int = typer.Option(None, help="A numeric tag ID"),
     query: str = typer.Option(None, help="A search query for the tag name")
 ):
+    """
+    Search for links with tags
+    """
     api = Search(base_url=shared_ctx['api_url'], api_token=shared_ctx['api_token'])
 
     if not exact and not query:
@@ -34,6 +37,9 @@ def by_list(
     exact: int = typer.Option(None, help="A numeric list ID"),
     query: str = typer.Option(None, help="A search query for the list name")
 ):
+    """
+    Search for links in lists
+    """
     api = Search(base_url=shared_ctx['api_url'], api_token=shared_ctx['api_token'])
 
     if not exact and not query:
@@ -52,6 +58,9 @@ def by_list(
 def by_query(
     query: str = typer.Option(...)
 ):
+    """
+    Search for tags by query
+    """
     api = Search(base_url=shared_ctx['api_url'], api_token=shared_ctx['api_token'])
     links = api.get_links_by_query(query)
     print_links(links)
