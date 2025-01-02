b1173223fc1c1e8d2518efcd0403d806267b4b8f
Evan Smith <esincork@gmail.com>
2025-01-02 13:47:16 +0000

    Update to latest python orb version (#9)
    * Update to latest python orb version
    
    * Remove outdated commands, and use poetry
    
    * fix linting

--- linkace_cli/cli/links.py
+++ linkace_cli/cli/links.py
@@ -156,7 +156,6 @@ def update(
         if val:
             link[key] = val
 
-
     link = {key: link[key] for key in provided_info.keys()}
     if interactive:
         link['tags'] = ", ".join([tag['name'] for tag in link['tags']])
